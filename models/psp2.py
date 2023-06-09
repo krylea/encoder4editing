e14b1d0bde04e489e10420ab493a7923d396f6a3
Kira Selby <kaselby@uwaterloo.ca>
2023-06-09 11:57:51 -0400

    refactor

--- models/psp2.py
+++ models/psp2.py
@@ -104,7 +104,7 @@ class pSp(nn.Module):
         elif self.opts.start_from_latent_avg:
             # Compute mean code based on a large number of latents (10,000 here)
             with torch.no_grad():
-                latent_in = torch.randn(10000, self.decoder.z_dim, device=self.decoder.parameters()[0].device)
+                latent_in = torch.randn(10000, self.decoder.z_dim, device=list(self.decoder.parameters())[0].device)
                 self.latent_avg = self.decoder.mapping(latent_in, None).mean(0, keepdim=True).to(self.opts.device)
         else:
             self.latent_avg = None
